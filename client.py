13ce80b6767b97cb9342d852ed928a1a9a250a13
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-11-07 12:33:48 +0530

    added some logs and error handling

--- client.py
+++ client.py
@@ -3,13 +3,30 @@ import imagezmq
 import socket
 import time
 import cv2
+import sys
 
-sender = imagezmq.ImageSender(connect_to="tcp://127.0.0.1:5555")
 
-rpiName = socket.gethostname()
-vs = VideoStream(src=0, resolution=(320, 240)).start()
-time.sleep(2.0)
+def pushFrames():
+    try:
+        print("Attempting to connect to imageHub")
+        sender = imagezmq.ImageSender(connect_to="tcp://127.0.0.1:5555")
+        print("Connected to image hub")
 
-while True:
-    frame = vs.read()
-    sender.send_image(rpiName, frame)
+        rpiName = socket.gethostname()
+
+        print("Starting video stream")
+        vs = VideoStream(src=0, resolution=(320, 240)).start()
+        # vs = VideoStream(usePiCamera=True).start()
+        time.sleep(2.0)
+
+        print("Begin sending frames")
+        while True:
+            frame = vs.read()
+            sender.send_image(rpiName, frame)
+    except:
+        print("error:")
+        raise
+
+
+if __name__ == "__main__":
+    pushFrames()
