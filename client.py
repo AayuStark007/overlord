2bf092a09d861cfbcbf4fbe32738468e49f41dad
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-11-07 12:49:14 +0530

    trying out the timeout decorator

--- client.py
+++ client.py
@@ -1,9 +1,38 @@
+from imagezmq.imagezmq import ImageSender
 from imutils.video import VideoStream
 import imagezmq
 import socket
 import time
 import cv2
 import sys
+
+from functools import wraps
+import errno
+import os
+import signal
+
+
+class TimeoutError(Exception):
+    pass
+
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
 
 
 def pushFrames():
@@ -21,11 +50,16 @@ def pushFrames():
 
         print("Begin sending frames")
         while True:
-            frame = vs.read()
-            sender.send_image(rpiName, frame)
+            readAndSend(rpiName, vs, sender)
     except:
         print("error:")
         raise
+
+
+@timeout(5)
+def readAndSend(hostName: str, vs: VideoStream, sender: ImageSender):
+    frame = vs.read()
+    sender.send_image(hostName, frame)
 
 
 if __name__ == "__main__":
